783934bc30f9b62028d4f6fa9b353e5b5e71854b
Darrell Rivera <darrell.rivera@livingsocial.com>
2014-09-24 20:01:58 -0400

    Add lists table and model and and add validations

---
+++ spec/models/list_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe List, :type => :model do
+  it "validates presence of name" do
+    list = List.new
+    expect(list.valid?).to eq(false)
+
+    list.name = "some name"
+    expect(list.valid?).to eq(true)
+  end
+
+  it "validates uniqueness of of name" do
+    name = "some name"
+    list = List.create!(:name => name)
+    same_name = List.new(:name => name)
+    expect(same_name.valid?).to eq(false)
+  end
+end
