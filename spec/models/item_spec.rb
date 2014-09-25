4d5dfd46648b6aaad44de140cf791289e32f06fd
Darrell Rivera <darrell.rivera@livingsocial.com>
2014-09-25 07:40:26 -0400

    Add validations to items and ahve it belong to list

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Item, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "validates presence of name" do
+    item = Item.new
+    expect(item.valid?).to eq(false)
+
+    item.name = "some name"
+    expect(item.valid?).to eq(true)
+  end
+
+  it "validates uniqueness of of name" do
+    name = "some name"
+    item = Item.create!(:name => name)
+    same_name = Item.new(:name => name)
+    expect(same_name.valid?).to eq(false)
+  end
+
+  it "belongs to a list" do
+    list = List.create!(:name => "a name")
+    item = list.items.new(:name => "item")
+    expect(item.list).to eq(list)
+  end
 end
